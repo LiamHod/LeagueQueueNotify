54593d40daac1b1af1b2ed055b9b75579d150942
Liam <canada8715@gmail.com>
2018-12-29 17:54:03 -0700

    Fixed detection and tried to implement testvideo

--- notify.py
+++ notify.py
@@ -13,24 +13,23 @@ from matplotlib import pyplot as plt
 
 def window_location():
     """Finds the location of the League window on the desktop"""
-    #while True:
-    leagueWin = winGuiAuto.findTopWindow("League of Legends")
-    #print(win32gui.GetWindowText(win32gui.GetForegroundWindow()))
-    #print("Visible?:", win32gui.IsWindowVisible(leagueWin))
-    if win32gui.IsWindowVisible(leagueWin) and not win32gui.IsIconic(leagueWin):
-        winLoc = win32gui.GetWindowPlacement(leagueWin)[-1]
-        grab_screen(winLoc)
-    else:
-        print("window not visible")
+    while True:
+        league_win = winGuiAuto.findTopWindow("League of Legends")
+        #print(win32gui.GetWindowText(win32gui.GetForegroundWindow()))
+        #print("Visible?:", win32gui.IsWindowVisible(league_win))
+        if win32gui.IsWindowVisible(league_win) and not win32gui.IsIconic(league_win):
+            win_loc = win32gui.GetWindowPlacement(league_win)[-1]
+            grab_screen(win_loc)
+        else:
+            print("window not visible")
 
 
 def grab_screen(coordinates):
     """Grabs a screenshot from the coordinated provided"""
     # sct = mss()
     with mss.mss() as sct:
-        print("League window coordinates: ", coordinates)
+        # print("League window coordinates: ", coordinates)
         sct_img = np.array(sct.grab(coordinates))
-        print(type(sct_img))
         detect_queue(sct_img)
         # mss.tools.to_png(sct_img.rgb, sct_img.size,level=6, output="testimage.png")   
 
@@ -38,16 +37,15 @@ def detect_queue(cur_img):
     """Used OpenCV to detect if the queue has popped in the image"""
     template = cv2.imread('templateimages\\winter_queue.png', 0)
     w, h = template.shape[::-1]
-    method = 'cv2.TM_CCOEFF_NORMED'
     grey_img = cv2.cvtColor(cur_img, cv2.COLOR_BGR2GRAY)
-    print(type(grey_img), type(template), type(method))
     res = cv2.matchTemplate(grey_img, template, cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     lol_loc = np.where(res >= threshold)
     for pt in zip(*lol_loc[::-1]):
-        cv2.rectangle(cur_img, pt, (pt[0]+ w, pt[1] + h), (0, 225, 255), 2)
-    cv2.imshow('Detected', cur_img)
-    cv2.waitKey(0)
+        if pt:
+            cv2.rectangle(cur_img, pt, (pt[0]+ w, pt[1] + h), (0, 225, 255), 2)
+            cv2.imshow('Detected', cur_img)
+            cv2.waitKey(0)
 #     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 #     top_left = max_loc
 #     bottom_right = (top_left[0] + w, top_left[1] + h)
